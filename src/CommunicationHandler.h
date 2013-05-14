73eaf3b8f9c40ccb62d647e9ac00e1d1e0d9ac41
Tomasz Zakrzewski <tz336079@students.mimuw.edu.pl>
2013-05-14 22:02:22 +0200

    Added empty classes and fixed CMakeLists.txt

---
+++ src/CommunicationHandler.h
@@ -0,0 +1,17 @@
+/* Tomasz [Tommalla] Zakrzewski, 2013
+All rights reserved */
+
+#ifndef COMMUNICATIONHANDLER_H
+#define COMMUNICATIONHANDLER_H
+#include <cstdio>
+#include "Singleton.h"
+
+/**
+ * @brief A Singleton class for the communication with the judge.
+ * Supposedly has the main loop (maintains controll)
+ **/
+class CommunicationHandler : public Singleton {
+
+};
+
+#endif // COMMUNICATIONHANDLER_H
---
+++ src/GTPParser.h
@@ -0,0 +1,11 @@
+/* Tomasz [Tommalla] Zakrzewski, 2013
+All rights reserved */
+
+#ifndef GTPPARSER_H
+#define GTPPARSER_H
+#include "Singleton.h"
+
+class GTPParser : public Singleton {
+};
+
+#endif // GTPPARSER_H
