a47e9ad1838473a680a16ad7de6e633fed0ade04
Tomasz Zakrzewski <tz336079@students.mimuw.edu.pl>
2013-05-21 19:40:02 +0200

    MCTNode implementation draft.

--- src/MCTNode.cpp
+++ src/MCTNode.cpp
@@ -3,6 +3,51 @@ All rights reserved */
 
 #include "MCTNode.h"
 
+void MCTNode::copyToSelf (const MCTNode& v) {
+	this->game = v.game;
+	this->isMax = v.isMax;
+	this->playsQty = v.playsQty;
+	this->playsWon = v.playsWon;
+	this->sons = v.sons;
+}
+
+
+MCTNode::MCTNode (const Game& game, const bool isMax) {
+	this->isMax = isMax;
+	this->game = game;
+	this->playsQty = 0;
+	this->playsWon = 0;
+}
+
+MCTNode::MCTNode (const MCTNode& v) {
+	this->copyToSelf(v);
+}
+
+const bool MCTNode::isLeaf() const {
+	return this->sons.empty();
+}
+
+void MCTNode::expand() {
+	//TODO implement expanding
+}
+
+bool MCTNode::playout() {
+	//TODO implement playout
+}
+
+MCTNode& MCTNode::operator= (const MCTNode& v) {
+	this->copyToSelf(v);
+	return *this;
+}
+
+MCTNode::~MCTNode() {
+	for(MCTNode* iter: this->sons)
+		delete iter;
+}
+
+
+
+
 
 
 
--- src/MCTNode.h
+++ src/MCTNode.h
@@ -3,7 +3,10 @@ All rights reserved */
 
 #ifndef MCTNODE_H
 #define MCTNODE_H
+#include <unordered_set>
 #include "../DiaballikEngine/src/Game.h"
+
+using namespace std;
 
 /**
  * @brief Monte Carlo Tree Node
@@ -11,10 +14,31 @@ All rights reserved */
 class MCTNode {
 	private:
 		Game game;
-		//TODO some container for sons, some values
+		unordered_set<MCTNode*> sons;
+		int playsWon, playsQty;
+		bool isMax;
+		
+		void copyToSelf(const MCTNode& v);
 	public:
-		MCTNode();
+		MCTNode(const Game &game, const bool isMax = true);
 		MCTNode(const MCTNode& v);
+		
+		/**
+		 * @brief Returns true if the node is a leaf in MCT
+		 **/
+		const bool isLeaf() const;
+		/**
+		 * @brief Expands the node
+		 * This method creates all possible sons for the node.
+		 * Bear in mind that it's use is *VERY* expensive, both time and memory-wise.
+		 **/
+		void expand();
+		/**
+		 * @brief Performs a playout starting from this node.
+		 * It implements the standard MCTS algorithm with UCB1
+		 * @return True if the playout was won by the player doing the first move
+		 **/
+		bool playout();
 		
 		MCTNode& operator=(const MCTNode& v);
 		~MCTNode();
