8bc8dea629b8c51fff482f99a8ebb4b24ee7076c
Tomasz Zakrzewski <tz336079@students.mimuw.edu.pl>
2013-05-23 23:55:32 +0200

    Some tweaks to constants.

--- src/botConstants.h
+++ src/botConstants.h
@@ -16,7 +16,7 @@ const int losePoints = 0;
 const int initialPlayoutsLimit = 2000;	//quantity of playouts for the whole tree at the beginning
 const int initialExpansionBorder = 15;
 
-const vector<pair<double, int> > expansionBorders({{0.0, 15}, {0.6, 14}});
+const vector<pair<double, int> > expansionBorders({{0.0, 15}, {0.7, 13}});
 const vector<pair<double, int> > playoutLimits({{0.0, 5000}, {0.7, 6000}, {0.85, 8000}});
 
 #endif
